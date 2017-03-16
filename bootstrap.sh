4dc62bbdc5f7aea40559aa66a203c61db2b783d8
Taylor B <bertiet@amazon.com>
2017-03-17 09:50:21 +1100

    added key generation logic

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,34 @@
-#!/bin/bash -xe
+#!/bin/bash -e
 if ! test -z $1
 then
     AWS_PROFILE="--profile $1"
 fi
+if test -e fullkey.pub
+then
+     grep -v "PUBLIC KEY" fullkey.pub > key.pub
+fi
+if ! test -e key.pub
+then
+    echo 'Generatoring public key as file key.pub does not exist, key will be found under "key.pem"...'
+    if ! test -e key.pem
+    then
+        openssl genrsa -out key.pem 2048
+    fi
+    openssl rsa -in key.pem -pubout > fullkey.pub
+    grep -v "PUBLIC KEY" fullkey.pub > key.pub
+fi
+echo 'Getting and deploying VPC IPv6 helper template...'
 wget https://s3-ap-southeast-2.amazonaws.com/vpc-ipv6-cfn-code-ap-southeast-2/template.yml
 aws cloudformation deploy --region ap-southeast-2 --template-file template.yml --stack-name vpc-ipv6-cfn --capabilities CAPABILITY_IAM $AWS_PROFILE || true
 aws cloudformation deploy --region ap-southeast-1 --template-file template.yml --stack-name vpc-ipv6-cfn --capabilities CAPABILITY_IAM $AWS_PROFILE || true
 rm -f template.yml
+
+echo 'Deploying base infrastructure template...'
+echo 'NOTE: You may receive approval emails for certificates, please approve to allow CFN to complete'
 aws cloudformation deploy --region ap-southeast-2 --template-file cftemplates/00-base.yaml --stack-name DockerMeetupBase --capabilities CAPABILITY_IAM --parameter-overrides  DNSHostedZone=aws.nkh.io ValidationDomain=nkh.io KeyPayload="$(cat key.pub | tr -d '\n')" $AWS_PROFILE || true
 aws cloudformation deploy --region ap-southeast-1 --template-file cftemplates/00-base.yaml --stack-name DockerMeetupBase2 --capabilities CAPABILITY_IAM --parameter-overrides DNSHostedZone=aws2.nkh.io ValidationDomain=nkh.io EnvironmentName=DockerMeetup2 KeyPayload="$(cat key.pub | tr -d '\n')" $AWS_PROFILE || true
+
+echo 'Preparing build environment...'
 aws cloudformation describe-stacks --region ap-southeast-2 --stack-name DockerMeetupBase $AWS_PROFILE > DockerMeetupBaseDescribe.json
 aws cloudformation describe-stacks --region ap-southeast-1 --stack-name DockerMeetupBase2 $AWS_PROFILE > DockerMeetupBase2Describe.json
 S3BUCKET=$(jq -r '.Stacks[] | .Outputs[] | select( .OutputKey | contains("S3Bucket")) | .OutputValue' DockerMeetupBaseDescribe.json)
@@ -25,6 +45,8 @@ ECRCREDITS2=$(jq -r '.Stacks[] | .Outputs[] | select( .OutputKey | contains("ECR
 ECRREDIRECT2=$(jq -r '.Stacks[] | .Outputs[] | select( .OutputKey | contains("ECRRedirect")) | .OutputValue' DockerMeetupBaseDescribe.json)
 rm -f DockerMeetupBaseDescribe.json
 rm -f DockerMeetupBase2Describe.json
+
+echo 'Building containers...'
 cp -f containers/AlienInvasion/game-sans-ajax.js containers/AlienInvasion/game.js
 docker build containers/AlienInvasion -t ${ECRALIEN}:sans
 docker tag ${ECRALIEN}:sans ${ECRALIEN2}:sans
@@ -36,6 +58,8 @@ docker build containers/postscore -t ${ECRPSCORE}:latest
 docker tag ${ECRPSCORE}:latest ${ECRPSCORE2}:latest
 docker build containers/redirect -t ${ECRREDIRECT}:latest
 docker tag ${ECRREDIRECT}:latest ${ECRREDIRECT2}:latest
+
+echo 'Pushing Containers...'
 $(aws ecr get-login --region ap-southeast-2 $AWS_PROFILE)
 docker push ${ECRALIEN}:sans
 docker push ${ECRALIEN}:ajax
@@ -47,6 +71,7 @@ docker push ${ECRALIEN2}:ajax
 docker push ${ECRPSCORE2}:latest
 docker push ${ECRREDIRECT}:latest
 
+echo 'Pushing files to S3..'
 aws s3 cp cftemplates/02-deployinfra.yaml s3://${S3BUCKET}/02-deployinfra.yaml --region ap-southeast-2 $AWS_PROFILE
 aws s3 cp cftemplates/02-deployinfra.yaml s3://${S3BUCKET2}/02-deployinfra.yaml --region ap-southeast-1 $AWS_PROFILE
 aws s3 cp cftemplates/03-addscores.yaml s3://${S3BUCKET}/03-addscores.yaml --region ap-southeast-2 $AWS_PROFILE
